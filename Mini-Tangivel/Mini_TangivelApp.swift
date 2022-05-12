15dc37decd0350852bf6cfa57e1751d6af9d64b5
marcoABZ <76532415+marcoABZ@users.noreply.github.com>
2022-05-12 18:31:48 -0300

    Initial Commit

---
+++ Mini-Tangivel/Mini_TangivelApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mini_TangivelApp.swift
+//  Mini-Tangivel
+//
+//  Created by Marco Zulian on 12/05/22.
+//
+
+import SwiftUI
+
+@main
+struct Mini_TangivelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
